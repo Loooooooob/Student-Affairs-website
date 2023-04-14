6423c75c8b367910beaa22c10d7d70c1b1410f8b
Tawfik Mohamed <92340572+Loooooooob@users.noreply.github.com>
2023-04-14 14:44:23 +0200

    Edit at Add Student Page

---
+++ add.js
@@ -0,0 +1,29 @@
+class student{
+    constructor(name,ID,Moblie,Email,DateOfBirth,Level,Department,Active,Gender) {
+        this.name=name;
+        this.ID=ID;
+        this.Moblie=Moblie;
+        this.Email=Email;
+        this.DateOfBirth=DateOfBirth;
+        this.Level=Level;
+        this.Department=Department;
+        this.Active=Active;
+        this.Gender=Gender;
+    }
+}
+function Set_Info(){
+    const Name=document.querySelector("#NameOfStudent").value;
+    const ID=document.querySelector("#ID").value;
+    const Moblie=document.querySelector("#Moblie").value;
+    const Email =document.querySelector("#Email ").value;
+    const DateOfBirth=document.querySelector("#Dateofbirth").value;
+    const Level=document.querySelector("#Level").value;
+    const Department =document.querySelector("#Department").value;
+    const Active=document.querySelector("#Active").value;    
+    const Gender=document.querySelector("#Gender").value;    
+    
+    const mystudent=new student(Name,ID,Moblie,Email,DateOfBirth,Level,Department,Active,Gender);
+    
+    localStorage.setItem("test",JSON.stringify(mystudent));
+};
+// localStorage.clear();
