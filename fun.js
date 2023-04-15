60d73db6460ef7022dda3e7fb119d624624c8b09
habibaelbehairy <habibaalaa03@gmail.com>
2023-04-15 23:40:03 +0200

    loginjs

--- fun.js
+++ fun.js
@@ -1,29 +1,58 @@
 function pop_up() {
   document.getElementById("login-modal").style.display = "block";
   document.getElementById("homeid").style.filter = "blur(5px)";
-
 }
+
 function close_modal() {
   document.getElementById("login-modal").style.display = "none";
   document.getElementById("homeid").style.filter = "blur(0px)";
+}
+function checkData() {
+  const userName = document.querySelector(".EnterUsername").value;
+  const password = document.querySelector(".EnterPassword").value;
+  if ((userName === "Tawfik" && password === "123456" )||(userName === "Habiba" && password === "1234567" )) {
+    close_modal();
+    localStorage.setItem("isloggedin", true);
+    switchbtn();
+  }
+  else {
+    alert("Username or password incorrect");
+  }
 }
 function goToPage() {
   var select = document.getElementById("studentoptions");
   var selectedOption = select.options[select.selectedIndex];
   if (selectedOption.value != "") {
-    window.location.href = selectedOption.value;
+    if (localStorage.getItem("isloggedin") === "true")  {
+      window.location.href = selectedOption.value;
+    } else {
+      alert("Please log in first");
+      pop_up();
+    }
   }
 }
-function checkData(){
-  const UserName=document.querySelector(".EnterUsername").value;
-  const Password=document.querySelector(".EnterPassword").value;
-  var Login=document.querySelector("#btn");
-  
-  if(UserName=="Tawfik" && Password=="123456"){
-    Login.innerHTML="Logout";
-    Login.style.backgroundColor="#F9F4ED";
-    Login.style.color="black";
-    close_modal();
+function switchbtn(){
+  if (localStorage.getItem("isloggedin") === "true") {
+    document.getElementById("btnlogin").style.zIndex = "1";
+    document.getElementById("btnlogout").style.zIndex = "2";
+  } 
+  else {
+    document.getElementById("btnlogin").style.zIndex = "2";
+    document.getElementById("btnlogout").style.zIndex = "1";
   }
 }
+function logout(){
+  localStorage.setItem("isloggedin", false);
+  switchbtn();
+}
+window.onload = function() {
+  switchbtn();
+};
+
+
+
+
+
+
+
 
