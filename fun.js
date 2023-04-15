826e350706ce36bcd3f8ee26963c22aea1688b11
habibaelbehairy <habibaalaa03@gmail.com>
2023-04-16 00:55:24 +0200

    search

--- fun.js
+++ fun.js
@@ -48,6 +48,28 @@ function logout(){
 window.onload = function() {
   switchbtn();
 };
+function Search() {
+
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchbar");
+  filter = input.value.toUpperCase();
+  table = document.getElementsByClassName("tabledb")[0];
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+
+
 
 
 
