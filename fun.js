822f3d2976c6684d9571df6fd76b702096aeff9e
habibaelbehairy <habibaalaa03@gmail.com>
2023-04-16 04:10:03 +0200

    push

--- fun.js
+++ fun.js
@@ -7,18 +7,6 @@ function close_modal() {
   document.getElementById("login-modal").style.display = "none";
   document.getElementById("homeid").style.filter = "blur(0px)";
 }
-function checkData() {
-  const userName = document.querySelector(".EnterUsername").value;
-  const password = document.querySelector(".EnterPassword").value;
-  if ((userName === "Tawfik" && password === "12345" )||(userName === "Habiba" && password === "123456" ) ||(userName === "Amira" && password === "1234567" )||(userName === "Omran" && password === "12345678" )||(userName === "Alaa" && password === "123456789" )||(userName === "Karem" && password === "1234567890" )) {
-    close_modal();
-    localStorage.setItem("isloggedin", true);
-    switchbtn();
-  }
-  else {
-    alert("Username or password incorrect");
-  }
-}
 
 function goToPage() {
   var select = document.getElementById("studentoptions");
@@ -32,6 +20,7 @@ function goToPage() {
     }
   }
 }
+
 function ChangePage() {
   var select = document.getElementById("databaseoptions");
   var selectedOption = select.options[select.selectedIndex];
@@ -42,6 +31,18 @@ function ChangePage() {
   }
 }
 
+function checkData() {
+  const userName = document.querySelector(".EnterUsername").value;
+  const password = document.querySelector(".EnterPassword").value;
+  if ((userName === "Tawfik" && password === "12345" )||(userName === "Habiba" && password === "123456" ) ||(userName === "Amira" && password === "1234567" )||(userName === "Omran" && password === "12345678" )||(userName === "Alaa" && password === "123456789" )||(userName === "Karem" && password === "1234567890" )) {
+    close_modal();
+    localStorage.setItem("isloggedin", true);
+    switchbtn();
+  }
+  else {
+    alert("Username or password incorrect");
+  }
+}
 
 function switchbtn(){
   if (localStorage.getItem("isloggedin") === "true") {
@@ -53,15 +54,26 @@ function switchbtn(){
     document.getElementById("btnlogout").style.zIndex = "1";
   }
 }
+
 function logout(){
   localStorage.setItem("isloggedin", false);
   switchbtn();
+  if (localStorage.getItem("isloggedin") === "false") {
+    window.location.href = "home.html"; 
+    switchbtn();
+  } else {
+    window.location.href = window.location.href;
+  }
 }
+
 window.onload = function() {
   switchbtn();
+  if (localStorage.getItem("isloggedin") !== "true" && window.location.href.indexOf("/home.html") === -1) {
+    window.location.href = "home.html";
+  }
 };
-function Search() {
 
+function Search() {
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("searchbar");
   filter = input.value.toUpperCase();
@@ -79,14 +91,3 @@ function Search() {
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
